7f34f531f238133737724ff8e265f29ae7abd5a3
Maureen0007 <maureen.wanjiku@student.moringaschool.com>
2022-06-19 00:22:38 +0300

    commit tree 73802969e841c3a4832f40304e3f3530dc34af97

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+
+function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+         method: 'POST',
+         headers: {
+             'content-type': 'application/json',
+             Accept: 'application/json',
+         },
+         body: JSON.stringify({
+             name: name,
+             email: email
+         })
+ 
+     })
+     .then(res => res.json())
+     .then(data => document.body.innerHTML = data.id)
+     .catch(err => document.body.append(err.message))
+  }
+ 
+ console.log(submitData("steve", "steve@steve.com"))
